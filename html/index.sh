ce3e6dc082826172c42f7e792e55bb7114b96cdf
Hugo <pi@hferee.hd.free.fr>
2014-12-30 16:57:29 +0100

    Added double quotes to protect variables

--- html/index.sh
+++ html/index.sh
@@ -4,13 +4,13 @@ echo "Recettes"             >> recipes.md;
 echo "========"             >> recipes.md;
 
 for i in $(  ls ../*.md | sed s/\.md// | sed 's/\.\.\///' | grep -v "README\|index" ); do
-  j=$( head -n 1 ../$i.md | sed s/\// );
-  for k in $( cat ../$i.md | sed "s/ /\n/g" | sed -n "/#/,/ /p" | sed "s/#//" ); do
-    echo "* [$j]($i.html)" >> index.$k.md;
+  j=$( head -n 1 ../"$i".md | sed s/\// );
+  for k in $( cat ../"$i".md | sed "s/ /\n/g" | sed -n "/#/,/ /p" | sed "s/#//" ); do
+    echo "* [$j]($i.html)" >> index."$k".md;
   done;
   echo "* [$j]($i.html)" >> recipes.md;
-  sed "1i[Retour à l'index](index.html)\n" ../$i.md > $i.md;
-  sed -i "s/#/\\\\#/g" $i.md;
+  sed "1i[Retour à l'index](index.html)\n" ../"$i".md > "$i".md;
+  sed -i "s/#/\\\\#/g" "$i".md;
 done
 
 echo "Mots clefs"              >> keywords.md;
@@ -18,6 +18,6 @@ echo "=========="              >> keywords.md;
 
 for i in $( ls *.md | grep -e "index\..*\.md" | sed 's/index\.//' | sed "s/\.md//" ); do
   echo "* [$i](index.$i.html)" >> keywords.md;
-  sed -i "1i[Retour à l'index](index.html)\n" index.$i.md;
+  sed -i "1i[Retour à l'index](index.html)\n" index."$i".md;
 done;
 
