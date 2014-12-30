3777855991fa49468e7a170ee783967c06823c0d
Hugo <pi@hferee.hd.free.fr>
2014-12-30 17:56:37 +0100

    [ Fix ]Dont repeat headers (due to 73deeb8).

--- html/postprod.sh
+++ html/postprod.sh
@@ -1,8 +1,8 @@
 cat header                   >> index.html
-echo "<div id=\"recipes\">"  >> index.html
-cat  recipes.html            >> index.html
-echo "</div>"                >> index.html
-echo "<div id=\"keywords\">" >> index.html
-cat keywords.html            >> index.html
-echo "</div>"                >> index.html
+echo '<div id="recipes">'	 >> index.html
+markdown recipes.md 		 >> index.html
+echo '</div>'				 >> index.html
+echo '<div id="keywords">'	 >> index.html
+markdown keywords.md 		 >> index.html
+echo '</div>'				 >> index.html
 cat footer                   >> index.html
