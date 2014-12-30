c210e733df458073012e4e9f8afd1102179693c9
gallais <guillaume.allais@ens-lyon.org>
2014-12-30 19:05:45 +0000

    [ cosmetic ] fixing spaces to make >> happy
    Reference : http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=690b0543a813b0ecfc51b0374c0ce6c8275435f0

--- html/postprod.sh
+++ html/postprod.sh
@@ -1,8 +1,8 @@
-cat header                   >> index.html
-echo '<div id="recipes">'	 >> index.html
-markdown recipes.md 		 >> index.html
-echo '</div>'				 >> index.html
-echo '<div id="keywords">'	 >> index.html
-markdown keywords.md 		 >> index.html
-echo '</div>'				 >> index.html
-cat footer                   >> index.html
+cat header                 >> index.html
+echo '<div id="recipes">'  >> index.html
+markdown recipes.md        >> index.html
+echo '</div>'              >> index.html
+echo '<div id="keywords">' >> index.html
+markdown keywords.md       >> index.html
+echo '</div>'              >> index.html
+cat footer                 >> index.html
